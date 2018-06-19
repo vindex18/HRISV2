e8f7a33d0945d8e707399fbf8a6c20f1ed2bdc5e
vindex18@gmail.com <myemail>
2018-06-19 18:27:42 +0800

    Latest FInal 06/19/18

--- src/Modules/Attendance/Dao/AttendanceDao.php
+++ src/Modules/Attendance/Dao/AttendanceDao.php
@@ -24,6 +24,76 @@ class AttendanceDao {
         //$queries = DB::getQueryLog(); var_dump($queries); die("End of Query");
     }
 
+    function insertAttendance($arr){
+        return AttendanceModel::firstOrcreate($arr)->save();
+    }
+
+    function addAttendance($type_id, $emp_id, $datetime){
+        $last_punch = AttendanceDao::getLastPunch($emp_id);
+        //echo $last_punch[0]->type_id; //die();
+        $arr = array('datetime' => $datetime,
+                     'emp_id' => $emp_id,
+                     'type_id' => (is_null($last_punch)) ? 1 : $type_id,
+                     'created_at' => date('Y-m-d H:i:s'),
+                     'updated_at' => date('Y-m-d H:i:s')
+                );
+        
+        if(strtotime($last_punch[0]->datetime)<strtotime($arr['datetime'])){ //Checking within Time line
+            if(is_null($last_punch[0]->type_id)){
+                return AttendanceDao::insertAttendance($arr);
+            }else{
+                switch($last_punch[0]->type_id){ //Checking Last Punch
+                    case 1: $qry = ($type_id=="2"||$type_id=="4") ? AttendanceDao::insertAttendance($arr) : false; // Only Break Out & Time Out
+                            break;
+
+                    case 2:  if($type_id=="3"){ //Only Break In 
+                                    $qry = AttendanceDao::insertAttendance($arr);
+                                    $msg = ($qry) ? "Breaked In Successfully! <br>".date('M d, Y g:i :A', strtotime($arr['datetime'])) : "Error Encountered on Break In! <br> ".date('M d, Y g:i :A', strtotime($arr['datetime']));
+                                }elseif($type_id=="4"){ //Time Out
+                                    $arr1 = array('datetime' => $datetime,
+                                                'emp_id' => $emp_id,
+                                                'type_id' => 3,
+                                                'created_at' => date('Y-m-d H:i:s'),
+                                                'updated_at' => date('Y-m-d H:i:s')
+                                                );
+
+                                    $qry = AttendanceDao::insertAttendance($arr1);
+                                    $qry = ($qry) ? AttendanceDao::insertAttendance($arr) : false;
+                                    $msg = ($qry) ?  "Break Out and Timed Out Successfully! <br>".date('M d, Y g:i :A', strtotime($arr['datetime'])) : "Error Encountered on Break Out and Time Out! <br> ".date('M d, Y g:i :A', strtotime($arr['datetime']));
+                                }else{
+                                    $qry = false;
+                                    $msg =  "Error Encountered on Invalid Data Submitted! <br> ".date('M d, Y g:i :A', strtotime($arr['datetime']));
+                                }
+                                break;
+
+                    case 3:     $qry = ($type_id=="4") ? AttendanceDao::insertAttendance($arr) : false; //Time Out
+                                $msg = ($qry) ?  "Timed Out Successfully! <br>".date('M d, Y g:i :A', strtotime($arr['datetime'])) : "Error Encountered on Time Out! <br> ".date('M d, Y g:i :A', strtotime($arr['datetime']));
+                                break;
+
+                    case 4:     $qry = ($type_id=="1") ? AttendanceDao::insertAttendance($arr) : false; //Only Time In
+                                $msg = ($qry) ? "Timed In Successfully! <br>".date('M d, Y g:i :A', strtotime($arr['datetime'])) : "Error Encountered on Time In! <br> ".date('M d, Y g:i :A', strtotime($arr['datetime']));
+                                break;
+
+                    default:    $qry = false; 
+                }
+            }
+        }else{
+            return array('msg' => 'Datetime Range Invalid!', 'status' => false);
+        }
+
+        return ($qry) ? array('msg' => $msg, 'status' => true) : array('msg' => $msg, 'status' => false);
+    }
+
+    function getLastPunch($emp_id){
+         return DB::table('attendance as a')
+                    ->select('datetime', 'type_id')
+                    ->where('emp_id', $emp_id)
+                    ->orderBy('datetime', 'DESC')
+                    ->take(1)
+                    ->get()
+                    ->toArray();
+    }
+
     function getDistinctDateAttendanceOfEmployee($from, $to, $emp_id){
         //DB::connection()->enableQueryLog();
         return DB::table('attendance as a')
@@ -36,9 +106,108 @@ class AttendanceDao {
         //$queries = DB::getQueryLog(); var_dump($queries); die("End of Query");
     }
 
-    function traverseRecords($data, $start_code, $start_datetime, $datetime, $ti, $to, $seq, $seqtime){
+    function findNextIndex($seq, $seqtime, $next_code, $start_index, $opp_code){
+        $start_index+=1;
 
-        $rec = array();
+        for($c=$start_index;$c<count($seq);$c++){
+            if($next_code==$seq[$c]){
+                return $c;
+            }elseif($seq[$c]==$opp_code){
+                
+            }
+        }
+        return null;
+    }
+
+    function traverseRecords($data, $start_code, $start_datetime, $datetime, $ti, $to, $seq, $seqtime, $seqindx){
+        $indx = array();
+
+        $brkincount = $brkoutcount = 0;
+
+        //Normal Seq
+        echo "<br>NORMAL Sequential<br>";
+        for($c=0;$c<count($data);$c++){
+            echo $data[$c]->code." - ".date('M d, Y g:i A', strtotime($data[$c]->datetime))."<br>";
+            ($data[$c]->code=="BI") ? $brkincount++ : null;
+            ($data[$c]->code=="BO") ? $brkoutcount++ : null ;
+        }
+
+        //Trace Seq
+        echo "<br>Sequential TI-TO<br>";
+        //Get Indexes Per Transaction
+
+        //First Get Time In -> Traverse Get Next Index
+        $start_code = "TI";
+        $next_code = "TO";
+        echo $seq[0]." - [0] ".date('M d, Y g:i A', strtotime($seqtime[0]))."<br>";
+
+        $c = 1;
+        while($c<count($data)){
+            echo $data[$c]->code." - [".$c."] ".date('M d, Y g:i A', strtotime($data[$c]->datetime))."<br>";
+
+            $c++;
+        }
+
+        /*while($c<count($seq)){
+                    
+            echo $seq[$c]." - [".$c."] ".date('M d, Y g:i A', strtotime($seqtime[$c]))."<br>";
+            //echo "<br>Next Code: ".$next_code."<br>";
+            if($next_code==$seq[$c]){
+                if($next_code=="TO"){
+                    $indx[] = [$c-1, $c];
+                    $next_code = "TI";
+                }elseif($next_code=="TI"){
+                    if($seq[$c]=="TI"){
+                        if(array_key_exists($c+1, $seq)){
+                            if($seq[$c+1]=="TO")
+                                $indx[] = [$c, $c+1];
+                            else
+                                $indx[] = [$c, '*'];
+                        }
+                        else{
+                            $indx[] = [$c, '*'];
+                        }
+                    }
+                }
+            }/*elseif($next_code=="TI"){
+                $indx[] = ['*', $c];
+            }
+            ++$c;
+        }*/
+        
+        for($c=0;$c<count($indx);$c++){
+            echo "<br><br>[".$c."]: ".$indx[$c][0]." - ".$indx[$c][1];
+        }
+
+        for($c=0;$c<count($seqindx);$c++){
+            echo $seqindx[$c]."<br>";
+            if(array_key_exists($c+1, $seqindx)){
+
+            }
+        }
+        echo "<br>BREAKOUT COUNT: ".$brkoutcount." - BREAKIN COUNT: ".$brkincount;
+        echo "<br><br>Calculating......<br>";
+        
+        for($c=0;$c<count($data);$c++){ 
+            $f = strtotime($data[$c]->datetime);
+            echo date('M d, Y g:i A', strtotime($data[$c]->datetime))." - ".$data[$c]->code." - ";
+            
+            if(array_key_exists($c+1, $data)){
+                $t = strtotime($data[$c+1]->datetime);
+                $diff = $t - $f;
+                $diff = AttendanceDao::convertTime($diff);
+                echo "- DIFFERENCE: ".$diff[0].", ".$diff[1].", ".$diff[2]."<br>"; 
+            }
+        }
+        
+        //echo "<br><br>----------TIME DIFF---------------------<br>";
+        //echo "<br>Total Hours: ".$total->diff($sum)->format('%h Hr %i Min %s Sec'); //%y Years %m Months %d Days 
+        //return $total->diff($sum)->format('%h Hours %i Minutes %s Seconds');
+        //echo "<br>".date('Y-m-j', strtotime('3 weekdays')); die(); I can use this function
+        //echo "<br>Total Hours: ".$total->diff($sum)->format('%y Years %m Months %d Days %h Hours %i Minutes %s Seconds');
+        die('<br>--------------------------------');    
+
+             /*$rec = array();
         //Normal Seq
         echo "<br>NORMAL Sequential<br>";
         for($c=0;$c<count($data);$c++){
@@ -52,28 +221,26 @@ class AttendanceDao {
 
         echo "<br>Calculating......<br>";
 
-        for($c=0;$c<count($seqtime);$c++){
+        for($c=0;$c<count($seqtime);$c++){ //or $data?
             $f = strtotime($seqtime[$c]);
             echo date('M d, Y g:i A', strtotime($seqtime[$c]))." - ".$seq[$c]." - ";
           
             if(array_key_exists($c+1, $seqtime)){
                 $fs = $seq[$c];
                 $sc = $seq[$c+1];
-                if($fs=="TI"&&$sc=="TO"){
-                    // $grpseq[$c] = [$fs, $sc];
-                    // $grptime[$c] = [$seqtime[$c], $seqtime[$c+1]];
-                    echo " OK ";
-                }else{
-                    if($fs!="TI")
-                        
+           
+                if($fs!="TI"){
 
-                    if($sc!="TO")
+                }
+
+                if($fs!="TO"){
+
                 }
 
                 $t = strtotime($seqtime[$c+1]);
                 $diff = $t - $f;
                 $diff = AttendanceDao::convertTime($diff);
-                echo "- DIFFERENCE: ".($diff)."<br>"; 
+                echo "- DIFFERENCE: ".$diff[0].", ".$diff[1].", ".$diff[2]."<br>"; 
                 echo $fs." - ".$sc."<br>";   
             }
         }
@@ -82,13 +249,13 @@ class AttendanceDao {
         //echo "<br>Total Hours: ".$total->diff($sum)->format('%h Hr %i Min %s Sec'); //%y Years %m Months %d Days 
         //return $total->diff($sum)->format('%h Hours %i Minutes %s Seconds');
         //echo "<br>".date('Y-m-j', strtotime('3 weekdays')); die(); I can use this function
-        //echo "<br>Total Hours: ".$total->diff($sum)->format('%y Years %m Months %d Days %h Hours %i Minutes %s Seconds');
+        //echo "<br>Total Hours: ".$total->diff($sum)->format('%y Years %m Months %d Days %h Hours %i Minutes %s Seconds');*/
         die('<br>--------------------------------');    
     }
 
     function convertTime($s) { 
-        $m = 0; $hr = 0; $td = "now";
-
+        /*$m = 0; $hr = 0; $td = "now";
+        
         if($s > 59){ 
             $m = (int)($s/60); 
             $s = $s-($m*60); // sec left over 
@@ -106,24 +273,40 @@ class AttendanceDao {
                 $td .= ", $m min";
         } 
     
-        return $td; 
+        return $td;*/ 
+        $m = 0; $hr = 0; 
+        
+        if($s > 59){ 
+            $m = (int)($s/60); 
+            $s = $s-($m*60); // sec left over 
+        } 
+
+        if($m>59){ 
+            $hr = (int)($m / 60); 
+            $m = $m - ($hr*60); // min left over 
+        } 
+    
+        return [$hr, $m, $s];
     } 
 
     function getEmployeeAttendance($from, $to, $emp_id){
 
         //Select MIN (TI) between 2 dates
         //$start = AttendanceDao::getMinTimeInBetweenDatetime($from, $to, $emp_id);
-        
+        //DB::connection()->enableQueryLog();
         $data = DB::table('employees AS e')
                     ->select('e.first_name', 'e.last_name', 'e.is_active', 'e.pos_title', 'a.datetime', 'a.emp_id', 't.code', 't.description')
                     ->leftJoin('attendance AS a' , 'e.id', '=', 'a.emp_id')
                     ->leftJoin('attendancetype as t', 'a.type_id', '=', 't.id')
                     ->where('e.id', '=', $emp_id)
-                    ->whereBetween('datetime', [$from, $to])
+                    //->whereBetween('DATE(datetime) as datetime', [$from, $to])
+                    ->whereDate('datetime', '>', $from)
+                    ->whereDate('datetime', '<', $to)
                     ->orderBy('datetime', 'ASC')
                     ->get()
                     ->toArray();
-
+        //$queries = DB::getQueryLog(); var_dump($queries); die("End of Query");
+        //var_dump($data); die();
         $ti = $to = $datetime = $seq = $seqtime = $preset = array();
         $brkincount = $brkoutcount = 0;
 
@@ -136,6 +319,7 @@ class AttendanceDao {
 
             if($data[$c]->code=="TI"||$data[$c]->code=="TO"){
                 $seq[] = $data[$c]->code;
+                $seqindx[] = $c;
                 $seqtime[] = $data[$c]->datetime;
             }
 
@@ -150,8 +334,8 @@ class AttendanceDao {
                 $to[$c] = $data[$c]->datetime;
         }
 
-        echo "<br>BRK-OUT-COUNT: ".$brkincount."<br>BRK-IN-COUNT: ".$brkoutcount."<br>"; 
-        echo "TIME-IN COUNT: ".count($ti)."<br>TIME-OUT COUNT: ".count($to)."<br><br>";
+        //echo "<br>BRK-OUT-COUNT: ".$brkincount."<br>BRK-IN-COUNT: ".$brkoutcount."<br>"; 
+        //echo "TIME-IN COUNT: ".count($ti)."<br>TIME-OUT COUNT: ".count($to)."<br><br>";
 
         //*** DEFICIENCY -> DECLARE INCOMPLETE 
 
@@ -160,7 +344,7 @@ class AttendanceDao {
             $start_datetime = $data[0]->datetime;
 
             //Traversing Through Records 
-            AttendanceDao::traverseRecords($data, $start_code, $start_datetime, $datetime, $ti, $to, $seq, $seqtime);
+            AttendanceDao::traverseRecords($data, $start_code, $start_datetime, $datetime, $ti, $to, $seq, $seqtime, $seqindx);
         }
 
         // //TEMP
